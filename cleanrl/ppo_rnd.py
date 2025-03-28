3dd43b7ec902f5d72b7a1a7111bf077d8169ae14
Nikhil Sridhar <niksrid@umich.edu>
2025-03-27 21:19:47 -0400

    calc and log intr reward

--- cleanrl/ppo_rnd.py
+++ cleanrl/ppo_rnd.py
@@ -88,6 +88,8 @@ class Args:
     """the dimension of the output RND embedding"""
     rnd_loss_coef: float = 1.0
     """the coefficient for the RND loss"""
+    rnd_reward_coef: float = 1.0
+    """the coefficient for the RND reward"""
     
 
 def make_env(env_id, idx, capture_video, run_name, gamma):
@@ -169,6 +171,9 @@ class RND(nn.Module):
     
     def get_prediction(self, x):
         return self.predictor(x)
+
+    def get_intr_reward(self, x):
+        return ((self.get_target(x) - self.get_prediction(x)) ** 2).mean(axis=-1)
     
         
 if __name__ == "__main__":
@@ -214,11 +219,12 @@ if __name__ == "__main__":
     optimizer = optim.Adam(list(agent.parameters()) + list(rnd.parameters()), lr=args.learning_rate, eps=1e-5)
 
     # ALGO Logic: Storage setup
-    # extra obs for RND
+    # extra observation for calculating intrinsic reward at last step
     obs = torch.zeros((args.num_steps+1, args.num_envs) + envs.single_observation_space.shape).to(device)
     actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
     logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
     rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    rewards_intr = torch.zeros((args.num_steps, args.num_envs)).to(device)
     dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
     values = torch.zeros((args.num_steps, args.num_envs)).to(device)
 
@@ -237,31 +243,39 @@ if __name__ == "__main__":
             lrnow = frac * args.learning_rate
             optimizer.param_groups[0]["lr"] = lrnow
 
-        for step in range(0, args.num_steps):
-            global_step += args.num_envs
-            obs[step] = next_obs
-            dones[step] = next_done
+        with torch.no_grad():
+            start_step = torch.zeros(args.num_envs, dtype=torch.int32)
+            for step in range(0, args.num_steps):
+                global_step += args.num_envs
+                obs[step] = next_obs
+                dones[step] = next_done
 
-            # ALGO LOGIC: action logic
-            with torch.no_grad():
+                # ALGO LOGIC: action logic
                 action, logprob, _, value = agent.get_action_and_value(next_obs)
                 values[step] = value.flatten()
-            actions[step] = action
-            logprobs[step] = logprob
+                actions[step] = action
+                logprobs[step] = logprob
 
-            # TRY NOT TO MODIFY: execute the game and log data.
-            next_obs, reward, terminations, truncations, infos = envs.step(action.cpu().numpy())
-            next_done = np.logical_or(terminations, truncations)
-            rewards[step] = torch.tensor(reward).to(device).view(-1)
-            next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
+                # TRY NOT TO MODIFY: execute the game and log data.
+                next_obs, reward, terminations, truncations, infos = envs.step(action.cpu().numpy())
+                next_done = np.logical_or(terminations, truncations)
+                rewards[step] = torch.tensor(reward).to(device).view(-1)
+                next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
 
-            if "final_info" in infos:
-                for info in infos["final_info"]:
-                    if info and "episode" in info:
-                        print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
-                        writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
-                        writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
-        obs[-1] = next_obs # save last obs for RND
+                # Generate intrinsic reward
+                reward_intr = args.rnd_reward_coef * rnd.get_intr_reward(next_obs)
+                rewards_intr[step] = reward_intr
+
+                if "final_info" in infos:
+                    for i, info in enumerate(infos["final_info"]):
+                        if info and "episode" in info:
+                            print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                            writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                            writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+                            episodic_return_intr = rewards_intr[start_step[i]:step+1, i].sum().item()
+                            writer.add_scalar("charts/episodic_return_intr", episodic_return_intr, global_step)
+                            start_step[i] = step + 1
+        obs[-1] = next_obs
 
         # bootstrap value if not done
         with torch.no_grad():
@@ -331,10 +345,10 @@ if __name__ == "__main__":
                     v_loss = 0.5 * ((newvalue - b_returns[mb_inds]) ** 2).mean()
 
                 # RND loss
-                rnd_loss = ((rnd.get_target(b_obs[mb_inds+1]) - rnd.get_prediction(b_obs[mb_inds+1])) ** 2).mean()
+                rnd_loss = rnd.get_intr_reward(b_obs[mb_inds+1]).mean()
 
                 entropy_loss = entropy.mean()
-                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef + rnd_loss * args.rnd_loss_coef
+                loss = pg_loss - args.ent_coef * entropy_loss + v_loss * args.vf_coef + args.rnd_loss_coef * rnd_loss
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -348,9 +362,6 @@ if __name__ == "__main__":
         var_y = np.var(y_true)
         explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
-        # for RND debugging 
-        rnd_loss_init_state = ((rnd.get_target(obs_init) - rnd.get_prediction(obs_init)) ** 2).mean()
-
         # TRY NOT TO MODIFY: record rewards for plotting purposes
         writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
         writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
@@ -361,7 +372,8 @@ if __name__ == "__main__":
         writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
         writer.add_scalar("losses/explained_variance", explained_var, global_step)
         writer.add_scalar("losses/rnd_loss", rnd_loss.item(), global_step)
-        writer.add_scalar("losses/rnd_loss_init_state", rnd_loss_init_state.item(), global_step)
+        with torch.no_grad():
+            writer.add_scalar("losses/rnd_loss_init_state", rnd.get_intr_reward(obs_init).mean().item(), global_step)
         print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
